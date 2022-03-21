69059a27f2148a7c3cc1e108557880ae8f31d2ba
unknown <kansh1111@yahoo.com>
2022-03-21 16:21:54 +0800

    Fixing

--- bookcafe/settings.py
+++ bookcafe/settings.py
@@ -88,17 +88,12 @@ WSGI_APPLICATION = 'bookcafe.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': config('DB_NAME'),
         'USER':config('DB_USER'),
         'PASSWORD': config('DB_PASSWORD'),
     }
 }
-
-
-# db_from_env = dj_database_url.config(conn_max_age=600)
-# DATABASES['default'].update(db_from_env)
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
