23779522f93be8ad737f1ece0ab28b3d1f7c1074
unknown <kansh1111@yahoo.com>
2022-04-13 10:25:47 +0800

    Testing

--- bookcafe/settings.py
+++ bookcafe/settings.py
@@ -23,7 +23,7 @@ STATIC_DIR = os.path.join(BASE_DIR, 'static')
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-DEBUG = False
+DEBUG = True
 
 SECRET_KEY = 'django-insecure-vm4s-b6uzeo_i+cgw_#qkv+ng69&e3k%w=5xig4gar06xsdk_-'
 
